bf12ded1dfbdc4e473319dc5eecdce24ad34020f
KateZin <90635745+KateZin@users.noreply.github.com>
2025-06-22 17:38:11 +0300

    fix validator_iterator

--- yolo_validator_iterator.py
+++ yolo_validator_iterator.py
@@ -2,6 +2,7 @@ import os
 import shutil
 from ultralytics import YOLO
 
+
 def delete_files_in_folder(folder_path):
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
@@ -9,29 +10,47 @@ def delete_files_in_folder(folder_path):
             if os.path.isfile(file_path):
                 os.remove(file_path)
         except Exception as e:
-            print(f'Ошибка при удалении файла {file_path}. {e}')
+            print(f'Ошибка при удалении файла {file_path}: {e}')
 
 
-folder_all = 'C:/Users/z.kate/source/GitHubRepos/ELC_Fall_git/YOLO_SAM/datasets/valid_all/'
-target_folder = 'C:/Users/z.kate/source/GitHubRepos/ELC_Fall_git/YOLO_SAM/datasets/valid/'
-images_folder = folder_all + 'images/'
-labels_folder = folder_all + 'labels/'
-image_files = os.listdir(images_folder)
-labels_files = os.listdir(labels_folder)
-image_files.sort()
-labels_files.sort()
+# Пути к папкам
+FOLDER_ALL = 'C:/Users/z.kate/source/GitHubRepos/ELC_Fall_git/YOLO_SAM/datasets/valid_all/'
+TARGET_FOLDER = 'C:/Users/z.kate/source/GitHubRepos/ELC_Fall_git/YOLO_SAM/datasets/valid/'
+
+IMAGES_FOLDER = os.path.join(FOLDER_ALL, 'images')
+LABELS_FOLDER = os.path.join(FOLDER_ALL, 'labels')
+
+TARGET_IMAGES_FOLDER = os.path.join(TARGET_FOLDER, 'images')
+TARGET_LABELS_FOLDER = os.path.join(TARGET_FOLDER, 'labels')
+
+image_files = sorted(os.listdir(IMAGES_FOLDER))
+label_files = sorted(os.listdir(LABELS_FOLDER))
 
 model = YOLO('./weights/best_large_val50_150.pt')
 
-for image, label in zip(image_files, labels_files):
-    target_images_folder = target_folder + 'images/'
-    target_labels_folder = target_folder + 'labels/'
-    prev_image = os.listdir(target_images_folder)
-    prev_label = os.listdir(target_labels_folder)
-    if len(prev_image) != 0:
-        shutil.move(target_images_folder + prev_image[0], images_folder + prev_image[0])
-        shutil.move(target_labels_folder + prev_label[0], labels_folder + prev_label[0])
-    shutil.move(images_folder + image, target_images_folder + image)
-    shutil.move(labels_folder + label, target_labels_folder + label)
-    metrics = model.val()
+for image_file, label_file in zip(image_files, label_files):
+    # Получаем текущие содержимые целевых папок
+    prev_images = os.listdir(TARGET_IMAGES_FOLDER)
+    prev_labels = os.listdir(TARGET_LABELS_FOLDER)
 
+    if prev_images:
+        shutil.move(
+            os.path.join(TARGET_IMAGES_FOLDER, prev_images[0]),
+            os.path.join(IMAGES_FOLDER, prev_images[0])
+        )
+    if prev_labels:
+        shutil.move(
+            os.path.join(TARGET_LABELS_FOLDER, prev_labels[0]),
+            os.path.join(LABELS_FOLDER, prev_labels[0])
+        )
+
+    shutil.move(
+        os.path.join(IMAGES_FOLDER, image_file),
+        os.path.join(TARGET_IMAGES_FOLDER, image_file)
+    )
+    shutil.move(
+        os.path.join(LABELS_FOLDER, label_file),
+        os.path.join(TARGET_LABELS_FOLDER, label_file)
+    )
+
+    model.val()
